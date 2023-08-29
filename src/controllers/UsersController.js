b7693b6703c9fd8ae29b11d62f850426b496b371
lucaslira <lucasliraone@gmail.com>
2023-08-29 14:04:12 -0300

    exception handling

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError");
+
 class UsersController{
     create(request, response){
         const { name, email, password } = request.body;
+
+        if(!name){
+            throw new AppError("Name is required!");
+        }
 
         response.json({ name, email, password });
     }
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require("express-async-errors");
+const AppError = require("./utils/AppError");
 const express = require("express");
 const app = express();
 const routes = require("./routes");
@@ -5,4 +7,17 @@ const PORT = 3333;
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            "status": "error",
+            "message": error.message
+        });
+    }
+
+    return response.status(500).json({
+        "status": "error",
+        "message": "Internal server error"
+    });
+});
 app.listen(PORT);
