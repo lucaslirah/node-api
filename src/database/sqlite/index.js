7b92b8a12a69e83dd54966746ec0a16419bda620
lucaslira <lucasliraone@gmail.com>
2023-08-29 16:53:42 -0300

    starting database

---
+++ src/database/sqlite/index.js
@@ -0,0 +1,3 @@
+const sqlite3 = require("sqlite3");
+const sqlite = require("sqlite");
+const path = require("path");
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+const database = require("./database/sqlite");
 const AppError = require("./utils/AppError");
 const express = require("express");
 const app = express();
@@ -7,6 +8,9 @@ const PORT = 3333;
 
 app.use(express.json());
 app.use(routes);
+
+database();
+
 app.use((error, request, response, next) => {
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
