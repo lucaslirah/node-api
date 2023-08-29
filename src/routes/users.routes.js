fdee0fe1398474e52084f08e364326b6ffc175d5
lucaslira <lucasliraone@gmail.com>
2023-08-29 10:48:27 -0300

    structure reviewed

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,7 +5,7 @@ const usersRoutes = Router();
 usersRoutes.post("/", (request, response) => {
     const { name, email, password } = request.body;
 
-    response.json({name, email, password});
+    response.json({ name, email, password });
 });
 
 module.exports = usersRoutes;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const routes = require("./routes");
 const app = express();
+const routes = require("./routes");
+const PORT = 3333;
 
 app.use(express.json());
 app.use(routes);
-
-const PORT = 3333;
-app.listen(PORT, () => console.log(`Server running on: ${PORT}`));
+app.listen(PORT);
