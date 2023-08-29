f48e2d71229f5ba437c0360844068106ee7b2841
lucaslira <lucasliraone@gmail.com>
2023-08-29 10:18:56 -0300

    organizing the project structure and creating the user routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
+const routes = require("./routes");
 const app = express();
 
-const PORT = 3333;
+app.use(express.json());
+app.use(routes);
 
-app.listen(PORT, () => console.log(PORT));
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server running on: ${PORT}`));
