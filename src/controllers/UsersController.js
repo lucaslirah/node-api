fb0ea578689227b67bbd4861bfa94afae6379d4f
lucaslira <lucasliraone@gmail.com>
2023-08-30 14:33:17 -0300

    user registration - create function

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,18 +1,34 @@
-const AppError = require("../utils/AppError");
 const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { hash, compare } = require("bcryptjs");
 
 class UsersController{
     async create(request, response){
         const { name, email, password } = request.body;
         
-        const users = await knex("users").where({ email });
-        console.log(email);
-        
-        if(users){
+        const usersEmails = await knex("users")
+        .select("email")
+        .where({email});
+
+        function userEmail(user){
+            return user.email === email;
+        }
+
+        const emailExists = usersEmails.find(userEmail);
+
+        if(emailExists){
             throw new AppError("Email already in use!");
         }
+
+        const hashedPassword = await hash(password, 8);
         
-        return response.json(users);
+        await knex("users").insert({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        return response.status(201).json();
     }
 }
 
