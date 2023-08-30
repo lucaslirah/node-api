d62c7f22d0d6ecb09b1b9bdc368e980388460106
lucaslira <lucasliraone@gmail.com>
2023-08-30 16:52:53 -0300

    refactoring UsersController create

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,29 +6,44 @@ class UsersController{
     async create(request, response){
         const { name, email, password } = request.body;
         
-        const usersEmails = await knex("users")
-        .select("email")
-        .where({email});
+        // const usersEmails = await knex("users")
+        // .select("email")
+        // .where({email});
 
-        function userEmail(user){
-            return user.email === email;
-        }
+        // function userEmail(user){
+        //     return user.email === email;
+        // }
 
-        const emailExists = usersEmails.find(userEmail);
+        // const emailExists = usersEmails.find(userEmail);
+
+        const [emailExists] = await knex("users").where({email});
 
         if(emailExists){
             throw new AppError("Email already in use!");
         }
 
-        const hashedPassword = await hash(password, 8);
+        // const hashedPassword = await hash(password, 8);
         
-        await knex("users").insert({
-            name,
-            email,
-            password: hashedPassword
-        });
+        // await knex("users").insert({
+        //     name,
+        //     email,
+        //     password: hashedPassword
+        // });
 
         return response.status(201).json();
+    }
+    async update(request, response){
+        const { name, email, password, old_password } = request.body;
+        const { id } = request.params;
+
+        const [user] = await knex("users")
+        .where({id});
+
+        if(!user){
+            throw new AppError("User not found!");
+        }
+        
+        response.json();
     }
 }
 
