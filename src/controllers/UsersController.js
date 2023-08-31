fce7ae2d28eee70317a4234c4a9df5296eec18fc
LucasLiraSilva <lucasliraone@gmail.com>
2023-08-30 22:01:27 -0300

    update function created - fecha pack installed

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,20 +1,11 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 const { hash, compare } = require("bcryptjs");
+const { format, setGlobalDateMasks } = require("fecha");
 
 class UsersController{
     async create(request, response){
         const { name, email, password } = request.body;
-        
-        // const usersEmails = await knex("users")
-        // .select("email")
-        // .where({email});
-
-        // function userEmail(user){
-        //     return user.email === email;
-        // }
-
-        // const emailExists = usersEmails.find(userEmail);
 
         const [emailExists] = await knex("users").where({email});
 
@@ -22,13 +13,13 @@ class UsersController{
             throw new AppError("Email already in use!");
         }
 
-        // const hashedPassword = await hash(password, 8);
+        const hashedPassword = await hash(password, 8);
         
-        // await knex("users").insert({
-        //     name,
-        //     email,
-        //     password: hashedPassword
-        // });
+        await knex("users").insert({
+            name,
+            email,
+            password: hashedPassword
+        });
 
         return response.status(201).json();
     }
@@ -42,8 +33,38 @@ class UsersController{
         if(!user){
             throw new AppError("User not found!");
         }
-        
+
+        const [userWithUpdatedEmail] = await knex("users").where({email});
+
+        // console.log(userWithUpdatedEmail)
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+            throw new AppError("Email already in use!");
+        }
+
+        // if(user.name === name && user.email === email){
+        //     throw new AppError("Data already registered");
+        // }
+
+        user.name = name;
+        user.email = email;
+
+        setGlobalDateMasks({
+            dateTimeMask: 'YYYY-MM-DD HH:mm:ss'
+        });
+        const timestamp = format(Date.now(), 'dateTimeMask');
+
+        await knex("users")
+        .where({id})
+        .update({
+            name: user.name,
+            email: user.email,
+            updated_at: timestamp,
+        })
+
+        // console.log(timestamp)
+
         response.json();
+
     }
 }
 
