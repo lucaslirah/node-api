70a9b883bcd32b9f1d73e68015de5bc6d0c66191
lucaslira <lucasliraone@gmail.com>
2023-09-01 10:55:18 -0300

    merge problem solved - update function finished

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -24,7 +24,52 @@ class UsersController{
         return response.status(201).json();
     }
     async update(request, response){
-        
+        const { name, email, password, old_password } = request.body;
+        const { id } = request.params;
+
+        const [user] = await knex("users").where({id});
+
+        if(!user){
+            throw new AppError("User not found!");
+        }
+        if(email){
+            const [userWithUpdatedEmail] = await knex("users").where({email});
+            
+            if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+                throw new AppError("Email already in use!");
+            }
+        }
+        if(password && !old_password){
+            throw new AppError("Old password is required!");
+        }
+        if(password && old_password){
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if(checkOldPassword){
+                user.password = await hash(password, 8);
+            }else{
+                throw new AppError("The old password does not match!");
+            }
+        }
+
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        setGlobalDateMasks({
+            dateTimeMask: 'YYYY-MM-DD HH:mm:ss'
+        });
+        const timestamp = format(Date.now(), 'dateTimeMask')
+
+        await knex("users")
+        .where({id})
+        .update({
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            updated_at: timestamp,
+        });
+
+        response.json();
     }
 }
 
